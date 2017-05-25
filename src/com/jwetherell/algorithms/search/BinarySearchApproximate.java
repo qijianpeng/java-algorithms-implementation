047fc39e0f2c87ef84d7ac04e99ad4f0f19565a9
qijianpeng <l_lzp@sina.com>
2017-05-25 15:47:26 +0800

    Change to java Template

--- src/com/jwetherell/algorithms/search/BinarySearchApproximate.java
+++ src/com/jwetherell/algorithms/search/BinarySearchApproximate.java
@@ -2,41 +2,33 @@ package com.jwetherell.algorithms.search;
 
 import org.junit.Test;
 
-public class BinarySearchApproximate {
+
+public class BinarySearchApproximate<T extends Comparable> {
 
     public static enum ORDER{
         ASCENDING, DESCENDING
     }
     private static final int SWITCH_TO_BRUTE_FORCE = 200;
 
-    private static int[] sorted = null;
+    //private static T[] sorted = null;
     private static ORDER order = ORDER.ASCENDING;
-
     // Assuming the array is sorted
-    public static final int find(int value, int[] array, ORDER order, boolean optimize) {
+    public static final  <T extends Comparable<T>>  int find(T value, T[] sorted, ORDER order, boolean optimize) {
         if (!BinarySearchApproximate.order.equals(order))BinarySearchApproximate.order = order;
-        BinarySearchApproximate.sorted = array;
         switch (order){
             case ASCENDING:
-                try {
-                    return recursiveFind(value, 0, BinarySearchApproximate.sorted.length - 1, optimize);
-                } finally {
-                    BinarySearchApproximate.sorted = null;
-                }
+                return recursiveFind(value,sorted, 0, sorted.length - 1, optimize);
             case DESCENDING:
-                try {
-                    return recursiveFindDescending(value, 0, BinarySearchApproximate.sorted.length - 1, optimize);
-                } finally {
-                    BinarySearchApproximate.sorted = null;
-                }
+                return recursiveFindDescending(value, sorted,0, sorted.length - 1, optimize);
         }
-        return Integer.MAX_VALUE;
+        return -1;
     }
 
-    private static int recursiveFindDescending(int value, int start, int end, boolean optimize) {
+    private static  <T extends Comparable<T>>  int recursiveFindDescending(T value, T[] sorted, int start,
+                                                                           int end, boolean optimize) {
         if (end - start <= 1) {
-            int lastValue = sorted[end]; // start==end
-            if (value >= lastValue)
+            T lastValue = sorted[end]; // start==end
+            if (value.compareTo(lastValue) >= 0)
                 return start; // start==end
 
             else return Integer.MAX_VALUE;
@@ -46,25 +38,26 @@ public class BinarySearchApproximate {
         int high = end + 1; // zero indexed, so add one.
         int middle = low + ((high - low) / 2);
 
-        int middleValue = sorted[middle];
-        if (value == middleValue)
+        T middleValue = sorted[middle];
+        if (value.equals(middleValue))
             return middle;
-        if (value > middleValue) {
+        if (value.compareTo(middleValue) > 0) {
             if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-                return linearSearchDescending(value, start, middle-1);
-            return recursiveFindDescending(value, start, middle-1, optimize);
+                return linearSearchDescending(value, sorted, start, middle-1);
+            return recursiveFindDescending(value, sorted, start, middle-1, optimize);
         }
         if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-            return linearSearchDescending(value, middle+1, end);
-        return recursiveFindDescending(value, middle+1, end, optimize);
+            return linearSearchDescending(value, sorted, middle+1, end);
+        return recursiveFindDescending(value, sorted,middle+1, end, optimize);
     }
 
 
 
-    private static int recursiveFind(int value, int start, int end, boolean optimize) {
+    private static  <T extends Comparable<T>>  int recursiveFind(T value, T[] sorted, int start,
+                                                                 int end, boolean optimize) {
         if (end - start <= 1) {
-            int lastValue = sorted[end]; // start==end
-            if (value <= lastValue)
+            T lastValue = sorted[end]; // start==end
+            if (value.compareTo(lastValue) <= 0)
                 return end; // start==end
 
             else return Integer.MAX_VALUE;
@@ -74,42 +67,44 @@ public class BinarySearchApproximate {
         int high = end + 1; // zero indexed, so add one.
         int middle = low + ((high - low) / 2);
 
-        int middleValue = sorted[middle];
-        if (value == middleValue)
+        T middleValue = sorted[middle];
+        if (value.equals(middleValue))
             return middle;
-        if (value > middleValue) {
+        if (value.compareTo(middleValue) > 0) {
             if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-                return linearSearch(value, middle + 1, end);
-            return recursiveFind(value, middle + 1, end, optimize);
+                return linearSearch(value,sorted, middle + 1, end);
+            return recursiveFind(value, sorted,middle + 1, end, optimize);
         }
         if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-            return linearSearch(value, start, middle - 1);
-        return recursiveFind(value, start, middle - 1, optimize);
+            return linearSearch(value, sorted, start, middle - 1);
+        return recursiveFind(value, sorted, start, middle - 1, optimize);
     }
 
-    private static final int linearSearch(int value, int start, int end) {
+    private static final  <T extends Comparable<T>>  int linearSearch(T value, T[] sorted, int start, int end) {
         for (int i = start; i <= end ; i++) {
-            int iValue = sorted[i];
-            if (value > iValue)continue;
+            T iValue = sorted[i];
+            if (value.compareTo(iValue) > 0)continue;
             else return i;
         }
         return Integer.MAX_VALUE;
     }
-    private static int linearSearchDescending(int value, int start, int end) {
+    private static  <T extends Comparable<T>>  int linearSearchDescending(T value, T[] sorted, int start, int end) {
         for (int i = start; i <= end ; i++) {
-            int iValue = sorted[i];
-            if (value < iValue)continue;
+            T iValue = sorted[i];
+            if (value.compareTo(iValue) < 0)continue;
             else return i;
         }
         return Integer.MAX_VALUE;
     }
-/*    @Test
+   @Test
     public void test(){
-        int[] array = {1, 4, 7, 8, 9,10};
+        Integer[] array = {1, 4, 7, 8, 9,10};
         Integer target = 10;
        // System.out.println(BinarySearchApproximate.find(target,array,false));
-        System.out.println(BinarySearchApproximate.find(target,array, ORDER.ASCENDING,false));
-        int[] arrayReverse = {10, 9, 8, 7, 4, 1};
-        System.out.println(BinarySearchApproximate.find(target,arrayReverse, ORDER.DESCENDING,false));
-    }*/
+        System.out.println(BinarySearchApproximate.find(target,array, ORDER.ASCENDING,true));
+        Integer[] arrayReverse = {10, 9, 8, 7, 4, 1};
+        System.out.println(BinarySearchApproximate.find(target,arrayReverse, ORDER.DESCENDING,true));
+    }
+
+
 }
