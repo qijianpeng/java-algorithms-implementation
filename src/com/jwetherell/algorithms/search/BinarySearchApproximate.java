7d0db16db049dce611b3c04061e213a57933d8ab
qijianpeng <l_lzp@sina.com>
2017-06-01 18:42:56 +0800

    Integrates with Comparator. Comparator.compare(o1,o2)>0 means o1 > o2, used in ascending order. otherwise use Collections.reversedOrder(comparator) to change.

--- src/com/jwetherell/algorithms/search/BinarySearchApproximate.java
+++ src/com/jwetherell/algorithms/search/BinarySearchApproximate.java
@@ -4,58 +4,25 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
 public class BinarySearchApproximate<T extends Comparable> {
 
-    public static enum ORDER{
-        ASCENDING, DESCENDING
-    }
+
     private static final int SWITCH_TO_BRUTE_FORCE = 200;
 
     //private static T[] sorted = null;
-    private static ORDER order = ORDER.ASCENDING;
     // Assuming the array is sorted
-    public static final  <T extends Comparable<T>>  int find(T value, T[] sorted, ORDER order, boolean optimize) {
-        if (!BinarySearchApproximate.order.equals(order))BinarySearchApproximate.order = order;
-        switch (order){
-            case ASCENDING:
-                return recursiveFind(value,sorted, 0, sorted.length - 1, optimize);
-            case DESCENDING:
-                return recursiveFindDescending(value, sorted,0, sorted.length - 1, optimize);
-        }
-        return -1;
+    public static final  <T extends Comparable<T>>  int find(T value, T[] sorted,
+                                                             Comparator<T> comparator, boolean optimize) {
+        return recursiveFind(value,sorted, 0, sorted.length - 1, comparator, optimize);
     }
 
 
 
-    private static  <T extends Comparable<T>>  int recursiveFindDescending(T value, T[] sorted, int start,
-                                                                           int end, boolean optimize) {
-        if (end - start <= 1) {
-            T lastValue = sorted[start]; // start==end
-            if (value.compareTo(lastValue) >= 0)
-                return start; // start==end
 
-            else return Integer.MAX_VALUE;
-        }
-
-        int low = start;
-        int high = end + 1; // zero indexed, so add one.
-        int middle = low + ((high - low) / 2);
-
-        T middleValue = sorted[middle];
-        if (value.equals(middleValue))
-            return middle;
-        if (value.compareTo(middleValue) > 0) {
-            if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-                return linearSearchDescending(value, sorted, start, middle-1);
-            return recursiveFindDescending(value, sorted, start, middle-1, optimize);
-        }
-        if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-            return linearSearchDescending(value, sorted, middle+1, end);
-        return recursiveFindDescending(value, sorted,middle+1, end, optimize);
-    }
 
     private static  <T extends Comparable<T>>  int recursiveFind(T value, T[] sorted, int start,
                                                                  int end, boolean optimize) {
@@ -84,6 +51,37 @@ public class BinarySearchApproximate<T extends Comparable> {
         return recursiveFind(value, sorted, start, middle - 1, optimize);
     }
 
+    private static  <T extends Comparable<T>>  int recursiveFind(T value, T[] sorted, int start,
+                                           int end, Comparator<T> comparator, boolean optimize) {
+        if (end - start <= 1) {
+            T rightValue = sorted[end]; // start==end
+            T leftValue = sorted[start];
+            if(comparator.compare(value, rightValue) <= 0 &&
+                    comparator.compare(value, leftValue) >= 0)return end;//ascending order
+            if (start == end){
+                if (comparator.compare(value, leftValue) <= 0) return end;
+                if (comparator.compare(value, leftValue) >= 0) return end +1;
+            }
+        }
+
+        int low = start;
+        int high = end + 1; // zero indexed, so add one.
+        int middle = low + ((high - low) / 2);
+
+        T middleValue = sorted[middle];
+
+        if (value.equals(middleValue))
+            return middle;
+        if (comparator.compare(value, middleValue) > 0) {
+            if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
+                return linearSearch(value, sorted, middle , end, comparator);
+            return recursiveFind(value, sorted, middle, end, comparator, optimize);
+        }
+        if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
+            return linearSearch(value, sorted, start, middle , comparator);
+        return recursiveFind(value, sorted, start, middle , comparator, optimize);
+    }
+
     private static final  <T extends Comparable<T>>  int linearSearch(T value, T[] sorted, int start, int end) {
         for (int i = start; i <= end ; i++) {
             T iValue = sorted[i];
@@ -92,61 +90,34 @@ public class BinarySearchApproximate<T extends Comparable> {
         }
         return Integer.MAX_VALUE;
     }
-    private static  <T extends Comparable<T>>  int linearSearchDescending(T value, T[] sorted, int start, int end) {
+    private static final  <T extends Comparable<T>>  int linearSearch(T value, T[] sorted,
+                                                                      int start, int end, Comparator<T> comparator) {
         for (int i = start; i <= end ; i++) {
             T iValue = sorted[i];
-            if (value.compareTo(iValue) < 0)continue;
+            if (comparator.compare(value, iValue) > 0)continue;
             else return i;
         }
         return Integer.MAX_VALUE;
     }
+
 //-----------List Methods
-    public static final  <T extends Comparable<T>>  int find(T value, List<T> sorted, ORDER order,
-                                                             boolean optimize) {
-        if (!BinarySearchApproximate.order.equals(order))BinarySearchApproximate.order = order;
-        switch (order){
-            case ASCENDING:
-                return recursiveFind(value,sorted, 0, sorted.size() - 1, optimize);
-            case DESCENDING:
-                return recursiveFindDescending(value, sorted,0, sorted.size() - 1, optimize);
-        }
-        return -1;
+    public static final  <T extends Comparable<T>>  int find(T value, List<T> sorted,
+                                                             Comparator<T> comparator, boolean optimize) {
+        return recursiveFind(value,sorted, 0, sorted.size() - 1, comparator, optimize);
     }
-    private static  <T extends Comparable<T>>  int recursiveFindDescending(T value, List<T> sorted, int start,
-                                                                           int end, boolean optimize) {
-        if (end - start <= 1) {
-            T lastValue = sorted.get(start); // start==end
-            if (value.compareTo(lastValue) >= 0)
-                return start; // start==end
 
-            else return Integer.MAX_VALUE;
-        }
-
-        int low = start;
-        int high = end + 1; // zero indexed, so add one.
-        int middle = low + ((high - low) / 2);
-
-        T middleValue = sorted.get(middle);
-        if (value.equals(middleValue))
-            return middle;
-        if (value.compareTo(middleValue) > 0) {
-            if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-                return linearSearchDescending(value, sorted, start, middle-1);
-            return recursiveFindDescending(value, sorted, start, middle-1, optimize);
-        }
-        if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-            return linearSearchDescending(value, sorted, middle+1, end);
-        return recursiveFindDescending(value, sorted,middle+1, end, optimize);
-    }
 
     private static  <T extends Comparable<T>>  int recursiveFind(T value, List<T> sorted, int start,
-                                                                 int end, boolean optimize) {
+                                                                 int end, Comparator<T> comparator, boolean optimize) {
         if (end - start <= 1) {
-            T lastValue = sorted.get(end); // start==end
-            if (value.compareTo(lastValue) <= 0)
-                return end; // start==end
-
-            else return Integer.MAX_VALUE;
+            T rightValue = sorted.get(end); // start==end
+            T leftValue = sorted.get(start);
+            if(comparator.compare(value, rightValue) <= 0 &&
+                    comparator.compare(value, leftValue) >= 0)return end;//ascending order
+            if (start == end){
+                if (comparator.compare(value, leftValue) <= 0) return end;
+                if (comparator.compare(value, leftValue) >= 0) return end +1;
+            }
         }
 
         int low = start;
@@ -154,57 +125,50 @@ public class BinarySearchApproximate<T extends Comparable> {
         int middle = low + ((high - low) / 2);
 
         T middleValue = sorted.get(middle);
+
         if (value.equals(middleValue))
             return middle;
-        if (value.compareTo(middleValue) > 0) {
+        if (comparator.compare(value, middleValue) > 0) {
             if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-                return linearSearch(value,sorted, middle + 1, end);
-            return recursiveFind(value, sorted,middle + 1, end, optimize);
+                return linearSearch(value,sorted, middle , end, comparator);
+            return recursiveFind(value, sorted,middle , end, comparator, optimize);
         }
         if (optimize && (end - middle) <= SWITCH_TO_BRUTE_FORCE)
-            return linearSearch(value, sorted, start, middle - 1);
-        return recursiveFind(value, sorted, start, middle - 1, optimize);
+            return linearSearch(value, sorted, start, middle , comparator);
+        return recursiveFind(value, sorted, start, middle , comparator, optimize);
     }
 
-    private static final  <T extends Comparable<T>>  int linearSearch(T value, List<T> sorted, int start, int end) {
+    private static final  <T extends Comparable<T>>  int linearSearch(T value, List<T> sorted,
+                                                                      int start, int end, Comparator comparator) {
         for (int i = start; i <= end ; i++) {
             T iValue = sorted.get(i);
-            if (value.compareTo(iValue) > 0)continue;
+            if (comparator.compare(value, iValue) > 0)continue;
             else return i;
         }
         return Integer.MAX_VALUE;
     }
-    private static  <T extends Comparable<T>>  int linearSearchDescending(T value, List<T> sorted, int start, int end) {
-        for (int i = start; i <= end ; i++) {
-            T iValue = sorted.get(i);
-            if (value.compareTo(iValue) < 0)continue;
-            else return i;
-        }
-        return Integer.MAX_VALUE;
-    }
-
 
 
 
    @Test
     public void test(){
-        Double[] array = {1.5, 4.4, 7.3, 8.7, 9.3, 10.4};
+        Double[] array = { 1.5, 4.4, 7.3, 8.7, 9.3, 10.4,11.0};
         Double target = 7.0;
+        Comparator<Double> c = new Comparator<Double>() {
+            @Override
+            public int compare(Double o1, Double o2) {
+                return o1.compareTo(o2);
+            }
+        };
        // System.out.println(BinarySearchApproximate.find(target,array,false));
-        System.out.println(BinarySearchApproximate.find(target,array, ORDER.ASCENDING,false));
+        System.out.println(BinarySearchApproximate.find(target,array,c,false));
         Double[] arrayReverse = {10.4, 9.3, 8.7, 7.3, 4.4, 1.5};
-        System.out.println(BinarySearchApproximate.find(target,arrayReverse, ORDER.DESCENDING,false));
+        System.out.println(BinarySearchApproximate.find(target,arrayReverse, Collections.reverseOrder(c),false));
 
         List<Double> list = Arrays.asList(array);
-        System.out.println(BinarySearchApproximate.find(target,list, ORDER.ASCENDING,false));
-        Collections.reverse(list);
-        System.out.println(BinarySearchApproximate.find(target,list, ORDER.DESCENDING,false));
-
-       Integer[] arrayInt = {1, 4, 7, 8, 9, 10};
-       Integer targetInt = 7;
-       System.out.println(BinarySearchApproximate.find(targetInt,arrayInt,ORDER.ASCENDING,false));
-
-
+        System.out.println(BinarySearchApproximate.find(target,list, c,false));
+        List<Double> listRervse = Arrays.asList(arrayReverse);
+        System.out.println(BinarySearchApproximate.find(target,listRervse,  Collections.reverseOrder(c),false));
    }
 
 
--- src/com/jwetherell/algorithms/search/inserts/BinarySearchInsert.java
+++ src/com/jwetherell/algorithms/search/inserts/BinarySearchInsert.java
@@ -5,31 +5,24 @@ import com.jwetherell.algorithms.search.BinarySearchApproximate;
 import org.junit.Test;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by qijianpeng on 26/05/2017.
  */
 public class BinarySearchInsert<T extends Comparable<T>> {
-    public static enum ORDER{
-        ASCENDING, DESCENDING
-    }
-    public static <T extends Comparable<T>>  boolean insert(T value, List<T> sortedList, ORDER order){
-       BinarySearchApproximate.ORDER or = BinarySearchApproximate.ORDER.ASCENDING;
-       if(order.equals(ORDER.DESCENDING))or = BinarySearchApproximate.ORDER.DESCENDING;
-       int index = BinarySearchApproximate.find(value, sortedList, or, true);
+
+    public static <T extends Comparable<T>>  boolean insert(T value, List<T> sortedList,
+                                                            Comparator<T> comparator){
+       int index = BinarySearchApproximate.find(value, sortedList, comparator, true);
        if ( index < 0) return false;
        sortedList.add(index, value); return true;
     }
 
     //TODO: To be fixed.
-    public static <T extends Comparable<T>>  boolean insert(T value, T[] sortedArrays, ORDER order){
-        BinarySearchApproximate.ORDER or = BinarySearchApproximate.ORDER.ASCENDING;
-        if(order.equals(ORDER.DESCENDING))or = BinarySearchApproximate.ORDER.DESCENDING;
-        int index = BinarySearchApproximate.find(value, sortedArrays, or, true);
+    public static <T extends Comparable<T>>  boolean insert(T value, T[] sortedArrays,
+                                                            Comparator<T> comparator){
+        int index = BinarySearchApproximate.find(value, sortedArrays, comparator, true);
         if ( index < 0) return false;
         Object[] newArray = new Object[sortedArrays.length+1];
         for(int i = sortedArrays.length-1; i >=index; i--){
@@ -51,8 +44,13 @@ public class BinarySearchInsert<T extends Comparable<T>> {
         list.add(9.3);
         list.add(10.4);
         Double target = 7.0;
-
-        insert(target,list,ORDER.ASCENDING);
+        Comparator<Double> c = new Comparator<Double>() {
+            @Override
+            public int compare(Double o1, Double o2) {
+                return o1.compareTo(o2);
+            }
+        };
+        insert(target,list,c);
         System.out.println(list);
     }
 
